28a64a566ecc238ccbc1f33cea5c9c55ddda5924
Gabriek0 <gabriel.sanches170@gmail.com>
2022-06-02 21:06:03 -0300

    feat: add fonts and configure document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,19 @@
-import Document from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Head>
+    </Html>
+  )
 }
+
+export default Document;
