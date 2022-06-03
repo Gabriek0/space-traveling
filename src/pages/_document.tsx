fa10e1c9e5831ce8e10652972230ef0ca889fad9
Gabriek0 <gabriel.sanches170@gmail.com>
2022-06-02 22:10:39 -0300

    feat: add header and style it

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,35 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 
-function Document() {
-  return (
-    <Html lang="pt-BR">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+class MyDocument extends Document {
+
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component,
+      });
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps;
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </Head>
-    </Html>
-  )
+      </Html >
+    )
+  }
 }
 
-export default Document;
+
+export default MyDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps } from 'next';
-
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+
+import styles from "./home.module.scss";
 
 interface Post {
   uid?: string;
@@ -24,9 +24,15 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+function Home() {
+  return (
+    <>
+      <div>Oi</div>
+    </>
+  )
+}
+
+export default Home;
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
